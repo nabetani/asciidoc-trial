c15c76c798181640f7add2ba57456e973f2c18af
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-06-11 22:55:35 +0900

    デバッグ出力撤去

--- lib/asciidoctor-pdf-table-keeptogether.rb
+++ lib/asciidoctor-pdf-table-keeptogether.rb
@@ -8,14 +8,11 @@ module Asciidoctor
 
       alias_method :convert_table_original, :convert_table
       def convert_table node
-        p [ node.caption, node.attr('nobreak') ]
         if three_state(node.attr('nobreak'), 'nobreak')
-          puts( "nobreak" )
           keep_together do
             convert_table_original node
           end
         else
-          puts( "standard" )
           convert_table_original node
         end
       end
